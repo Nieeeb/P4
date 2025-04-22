d30c2235fa881ce0f1ade04831d5416324de94c9
Nieeeb <william_nieboer@hotmail.com>
2025-04-22 13:00:23 +0200

    test on all training data

--- DAWIDD/independence_test_real.py
+++ DAWIDD/independence_test_real.py
@@ -7,7 +7,6 @@ import yaml
 import pandas as pd
 import tqdm
 
-
 #Loading args from CLI
 parser = argparse.ArgumentParser()
 parser.add_argument('--args_file', default='utils/args.yaml', type=str)
@@ -32,31 +31,31 @@ with open(args.args_file) as cf_file:
 sampling_rate_per_day = 3985
 
 samples_all_time = 749195
-ckpt = '/ceph/project/DAKI4-thermal-2025/P4/runs/ae_complex_full_1/100'
+
+ckpt = '/home/nieb/Projects/DAKI Projects/P4/DAWIDD/ae_complex'
+
 detectors = {
     'daily': DAWIDD_HSIC(ckpt, device='cuda', max_window_size=1*sampling_rate_per_day,
-                         min_window_size=int(0.8*1*sampling_rate_per_day), disable_drift_reset=True),
+                            min_window_size=int(0.8*1*sampling_rate_per_day), disable_drift_reset=True),
     'weekly': DAWIDD_HSIC(ckpt, device='cuda', max_window_size=7*sampling_rate_per_day,
-                          min_window_size=int(0.8*7*sampling_rate_per_day), disable_drift_reset=True),
+                            min_window_size=int(0.8*7*sampling_rate_per_day), disable_drift_reset=True),
     'monthly': DAWIDD_HSIC(ckpt, device='cuda', max_window_size=30*sampling_rate_per_day,
-                           min_window_size=int(0.8*30*sampling_rate_per_day), disable_drift_reset=True),
+                            min_window_size=int(0.8*30*sampling_rate_per_day), disable_drift_reset=True),
     'all_time': DAWIDD_HSIC(ckpt, device='cuda', max_window_size=1*samples_all_time,
-                             min_window_size=int(0.8*samples_all_time), disable_drift_reset=True)
+                            min_window_size=int(0.8*samples_all_time), disable_drift_reset=True)
 }
 
 starting_epoch = -1
 
-validation_loader, validation_sampler = prepare_loader(args, params,
-                            file_txt=params.get('val_txt'),
-                            img_folder=params.get('val_imgs'),
+train_loader, train_sampler = prepare_loader(args, params,
+                            file_txt=params.get('train_txt'),
+                            img_folder=params.get('train_imgs'),
                             starting_epoch=starting_epoch,
                             num_workers=16
                             )
 
-
-
 idx = 0
-for batch in tqdm.tqdm(validation_loader, desc="Validation batches"):
+for batch in tqdm.tqdm(train_loader, desc="Train batches"):
     images = batch[0]
     for img in images:
         for det in detectors.values():
@@ -66,6 +65,8 @@ for batch in tqdm.tqdm(validation_loader, desc="Validation batches"):
 # ---- Write CSV files ----
 for name, det in detectors.items():
     df = pd.DataFrame({'hsic_val': det.hsic_history})
-    out_fname = f"{name}_hsic_values.csv"
+    out_fname = os.path.join('DAWIDD', f"{name}_hsic_values.csv")
     df.to_csv(out_fname, index_label='sample_index')
     print(f"Wrote {out_fname} with {len(det.hsic_history)} entries")
+    
+print("---------------------------- Test Complete -------------------------------")
