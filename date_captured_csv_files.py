5203f9dd46acda3719c56243fea0b475e51f2e0b
VictorSteinrud <138033799+VictorSteinrud@users.noreply.github.com>
2025-04-23 11:04:00 +0200

    ækk

--- date_captured_csv_files.py
+++ date_captured_csv_files.py
@@ -1,19 +1,8 @@
 import json
 import pandas as pd
 
-import os
 
-base_dir = os.path.dirname(__file__)  
-# go up one level, then into harborfrontv2
-json_path = os.path.join(base_dir, '..', 'harborfrontv2', 'Train.json')
-
-# DEBUG: print out what we’re actually looking for
-print("Base directory:", base_dir)
-print("Resolved JSON path:", json_path)
-print("File exists?", os.path.exists(json_path))
-
-
-with open("/ceph/project/DAKI4-thermal-2025/harborfrontv2/Train.json", 'r') as file:
+with open("Train.json", 'r') as file:
     data = json.load(file)
 
 n_images = len(data.get("images"))
