8f97ab3dedba0c42488ae263debe730ea6f317bb
VictorSteinrud <138033799+VictorSteinrud@users.noreply.github.com>
2025-04-23 11:09:00 +0200

    with twdm

--- date_captured_csv_files.py
+++ date_captured_csv_files.py
@@ -1,14 +1,16 @@
 import json
 import pandas as pd
-
+from tqdm import tqdm  
 
 with open("Train.json", 'r') as file:
     data = json.load(file)
 
-n_images = len(data.get("images"))
+images = data.get("images", [])
+n_images = len(images)
 
-
-date_captured_list = [data.get("images")[i].get("date_captured") for i in range(n_images)]
+date_captured_list = []
+for img in tqdm(images, desc="Reading dates", unit="img"):
+    date_captured_list.append(img.get("date_captured"))
 
 df = pd.DataFrame(data=date_captured_list, columns=["date_captured"])
-df.to_csv("date_captured_train.csv")
+df.to_csv("date_captured_train.csv", index=False)
